d9ab8d426e2336ce7be5262d3ac60ec8b9c6de6a
Innocent1312 <anthonydev33@gmail.com>
2020-01-18 16:12:20 +0200

    123

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react';
 
 
-const Event = ({id, title, eventTimeRange, description}) => (
+const Event = ({ id, title, eventTimeRange, description }) => (
   <Card
     id={id}
     header={title}
