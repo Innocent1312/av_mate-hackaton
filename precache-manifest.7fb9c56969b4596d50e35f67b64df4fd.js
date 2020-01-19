85e721ac3dc560c42e78bfa1b80db33dcc877f86
Innocent1312 <anthonydev33@gmail.com>
2020-01-19 08:53:54 +0200

    Updates

---
+++ precache-manifest.7fb9c56969b4596d50e35f67b64df4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486ae7d39cc6176cc983fea80d4270ad",
+    "url": "/av_ma-hackaton/index.html"
+  },
+  {
+    "revision": "2b1ffd97826458658a76",
+    "url": "/av_ma-hackaton/static/css/main.c8699729.chunk.css"
+  },
+  {
+    "revision": "4f22e3ec22018200e8e2",
+    "url": "/av_ma-hackaton/static/js/2.761a3a73.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/av_ma-hackaton/static/js/2.761a3a73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b1ffd97826458658a76",
+    "url": "/av_ma-hackaton/static/js/main.b88ce552.chunk.js"
+  },
+  {
+    "revision": "93332afdf99666486365",
+    "url": "/av_ma-hackaton/static/js/runtime-main.21066cc9.js"
+  }
+]);
